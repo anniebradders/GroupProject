ffd96681f01fdf4d2f64650047eae1ce8f564dc4
annie bradley <74246591+anniebradders@users.noreply.github.com>
2020-11-26 18:06:49 +0000

    Add files via upload

---
+++ dbo.Employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Employee_ID]     INT            IDENTITY (1, 1) NOT NULL,
+    [Employee_F_Name] NCHAR (10)     NULL,
+    [Employee_L_Name] VARCHAR (50)   NULL,
+    [Employee_Email]  VARCHAR (50)   NULL,
+    [Employee_Pswd]   VARCHAR (50)   NULL,
+    [FileName]        NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Employee_ID] ASC)
+);
+
