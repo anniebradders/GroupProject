5be9ae2bd9c3e3857c2f48cd1658d4e000b3971f
annie bradley <74246591+anniebradders@users.noreply.github.com>
2020-12-09 21:25:26 +0000

    Delete dbo.Manager.sql

--- dbo.Manager.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Manager] (
-    [Manager_ID]       INT           IDENTITY (1, 1) NOT NULL,
-    [Manager_F_Name]   VARCHAR (50)  NOT NULL,
-    [Manager_L_Name]   NVARCHAR (50) NOT NULL,
-    [Manager_Username] VARCHAR (50)  NOT NULL,
-    [Manager_Pswd]     VARCHAR (50)  NOT NULL,
-    PRIMARY KEY CLUSTERED ([Manager_ID] ASC)
-);
-
