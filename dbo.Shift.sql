552982d513b599f62bfb81d179d5ad8cc9aaee55
annie bradley <74246591+anniebradders@users.noreply.github.com>
2020-12-09 21:25:35 +0000

    Delete dbo.Shift.sql

--- dbo.Shift.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Shift] (
-    [Shift_ID]     INT           IDENTITY (1, 1) NOT NULL,
-    [Shift_Date]   NVARCHAR (50) NOT NULL,
-    [Shift_S_Time] NCHAR (10)    NOT NULL,
-    [Shift_E_Time] NCHAR (10)    NOT NULL,
-    [Shift_Hour]   INT           NOT NULL,
-    PRIMARY KEY CLUSTERED ([Shift_ID] ASC)
-);
-
